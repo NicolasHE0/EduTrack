3bbeec82659641815b66a5d5ee3ede1dba2352ea
NicolasHE0 <henkernicolas@gmail.com>
2026-03-21 09:29:14 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,10 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("SW registrado"))
+      .catch(e => console.log("SW error:", e));
+  });
+}
